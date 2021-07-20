042f128222091833ba83c477c7b81caa988f9de1
hyderarain <harain@chicagobooth.edu>
2021-07-20 20:09:11 +0000

    Full Functional

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -1,23 +1,54 @@
 class ForexController < ApplicationController
   def list_from
-    render({:template => "list_from.html.erb"})
-  end
-  def list_to
-    @from_curr = params.fetch("from_curr")
-    require 'net/http'
-    require 'json'
-
-    url = "https://api.exchangerate.host/convert?from=USD&to=EUR"
+    url = "https://api.exchangerate.host/symbols"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     @response_obj = JSON.parse(response)
 
+    @sym_array = Array.new
 
+
+      sym_hash = @response_obj.fetch("symbols")
+      i = 0
+        sym_hash.keys.each do
+          @sym_array.push(sym_hash.keys[i])
+          i = i+1
+        end
+
+
+    render({:template => "list_from.html.erb"})
+  end
+  def list_to
+    @from_curr = params.fetch("from_curr")
+    url = "https://api.exchangerate.host/symbols"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @response_obj = JSON.parse(response)
+
+    @sym_array = Array.new
+
+
+      sym_hash = @response_obj.fetch("symbols")
+      i = 0
+        sym_hash.keys.each do
+          @sym_array.push(sym_hash.keys[i])
+          i = i+1
+        end
     render({:template => "list_to.html.erb"})
   end
   def convert
     @from_curr = params.fetch("from_curr")
     @to_curr = params.fetch("to_curr")
+
+    require 'net/http'
+    require 'json'
+
+    url = "https://api.exchangerate.host/convert?from=" + @from_curr + "&to=" + @to_curr
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+
+    @rate = response_obj.fetch("result")
     render({:template => "convert.html.erb"})
   end
 end
